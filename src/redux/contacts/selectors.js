fdd9e22e05e95bd14301596ee80bcf82c77cfd9f
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-03-21 23:57:06 +0200

    fix

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,8 +1,3 @@
 export const selectContacts = state => state.contacts.items;
 export const selectContactsFilter = state => state.filter;
 export const selectIsLoading = state => state.contacts.isLoading;
-// export const selectError = state => state.contacts.error;
-
-// export const selectAllContacts = state => state.contacts.items;
-// export const selectLoading = state => state.contacts.loading;
-// export const selectFilter = state => state.filter;
